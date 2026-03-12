ae5bf7ef00547a5286abaeeb4aff7d2cb1e85863
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-12 20:35:02 +0700

    middleware

--- src/application/web.js
+++ src/application/web.js
@@ -1,43 +1,6 @@
 import express from "express";
-import { prismaClient } from "./database";
-import { logger } from "./logging";
+import { publicRouter } from "../route/public-api";
 
 export const web = express();
-web.use(
-  express.json({
-    log: [
-      {
-        emit: "event",
-        level: "query",
-      },
-      {
-        emit: "event",
-        level: "error",
-      },
-      {
-        emit: "event",
-        level: "info",
-      },
-      {
-        emit: "event",
-        level: "warn",
-      },
-    ],
-  }),
-);
-
-prismaClient.$on("error", (e) => {
-  logger.error(e);
-});
-
-prismaClient.$on("warn", (e) => {
-  logger.warn(e);
-});
-
-prismaClient.$on("info", (e) => {
-  logger.info(e);
-});
-
-prismaClient.$on("query", (e) => {
-  logger.info(e);
-});
+web.use(express.json());
+web.use(publicRouter);
