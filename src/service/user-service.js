dbfe4bbcbb9b93d067b886825af73811e9693bc9
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-12 20:04:43 +0700

    user service

---
+++ src/service/user-service.js
@@ -0,0 +1,29 @@
+import { validate } from "../validation/user-validation";
+import { registerUserValidation } from "../validation/user-validation";
+import { prismaClient } from "../application/database";
+import { ResponseError } from "../error/response-error";
+import bycript from "bcrypt";
+
+const register = async () => {
+  const user = validate(registerUserValidation, request);
+
+  const countUser = await prismaClient.user.count({
+    where: {
+      username: user.username,
+    },
+  });
+
+  if (countUser === 1) {
+    throw new ResponseError(400, "User already exits");
+  }
+
+  user.password = await bycript.hash(user.password, 10);
+
+  return prismaClient.user.create({
+    data: user,
+    select: {
+      username: true,
+      name: true,
+    },
+  });
+};
