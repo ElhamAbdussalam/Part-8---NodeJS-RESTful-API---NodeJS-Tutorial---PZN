5e3fd6bdf25a32927c17b86ad27fb1572a769147
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-16 11:56:00 +0700

    contact test

--- test/contact.test.js
+++ test/contact.test.js
@@ -34,4 +34,19 @@ describe("POST /api/contacts", function () {
     expect(result.body.data.email).toBe("test@gmail.com");
     expect(result.body.data.phone).toBe("123123123");
   });
+
+  it("should reject if request is not valid", async () => {
+    const result = await supertest(web)
+      .post("/api/contacts")
+      .set("Authorization", "test")
+      .send({
+        first_name: "",
+        last_name: "test",
+        email: "test",
+        phone: "123123123123123123123123123",
+      });
+
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
+  });
 });
