21f014e9c3b1a97a5a4cd9adfac046230c6d7171
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-16 11:34:59 +0700

    contact controller

--- src/route/api.js
+++ src/route/api.js
@@ -1,6 +1,6 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
-import { authMiddleware } from "../controller/auth-middleware.js";
+import { authMiddleware } from "../middleware/auth-middleware.js";
 
 const userRouter = new express.Router();
 userRouter.use(authMiddleware);
