588b7a412d457fef77238715890552462eab9c2d
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-16 21:25:51 +0700

    address service

---
+++ src/service/address-service.js
@@ -0,0 +1,37 @@
+import prismaClient from "../application/database.js";
+const { validate } = require("uuid");
+const { getContactValidation } = require("../validation/contact-validation");
+import ResponseError from "../error/response-error.js";
+import { createAddressValidation } from "../validation/address-validation.js";
+
+const create = async (user, contactId, request) => {
+  contactId = validate(getContactValidation, contactId);
+
+  const totalContactInDatabase = await prismaClient.contact.count({
+    where: {
+      username: user.username,
+      id: contactId,
+    },
+  });
+
+  if (totalContactInDatabase !== 1) {
+    throw new ResponseError(404, "contact is not found");
+  }
+
+  const address = validate(createAddressValidation, request);
+  address.contact_id = contactId;
+
+  return prismaClient.address.create({
+    data: address,
+    select: {
+      id: true,
+      street: true,
+      city: true,
+      province: true,
+      country: true,
+      postal_code: true,
+    },
+  });
+};
+
+export default { create };
