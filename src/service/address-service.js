712cc32fadbc7d8532b82ea81cba9503b080c719
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-17 14:19:26 +0700

    address service

--- src/service/address-service.js
+++ src/service/address-service.js
@@ -5,6 +5,7 @@ import { ResponseError } from "../error/response-error.js";
 import {
   createAddressValidation,
   getAddressValidation,
+  updateAddressValidation,
 } from "../validation/address-validation.js";
 
 const checkContactMustExits = async (user, contactId) => {
@@ -68,4 +69,41 @@ const get = async (user, contactId, addressId) => {
   return address;
 };
 
-export default { create, get };
+const update = async (user, contactId, request) => {
+  contactId = await checkContactMustExits(user, contactId);
+  const address = validate(updateAddressValidation, request);
+
+  const totalAddressInDatabase = await prismaClient.address.count({
+    where: {
+      contact_id: contactId,
+      id: address.id,
+    },
+  });
+
+  if (totalAddressInDatabase !== 1) {
+    throw new ResponseError(404, "Address is not found");
+  }
+
+  return prismaClient.address.update({
+    where: {
+      id: address.id,
+    },
+    data: {
+      street: address.street,
+      city: address.city,
+      province: address.province,
+      country: address.country,
+      postal_code: address.postal_code,
+    },
+    select: {
+      id: true,
+      street: true,
+      city: true,
+      province: true,
+      country: true,
+      postal_code: true,
+    },
+  });
+};
+
+export default { create, get, update };
