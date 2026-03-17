2d28e1cc49a959d8c25ce21bc57bb89fb84d4334
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-17 14:53:30 +0700

    remove address

--- src/service/address-service.js
+++ src/service/address-service.js
@@ -106,4 +106,26 @@ const update = async (user, contactId, request) => {
   });
 };
 
-export default { create, get, update };
+const remove = async (user, contactId, addressId) => {
+  contactId = await checkContactMustExits(user, contactId);
+  addressId = validate(getAddressValidation, addressId);
+
+  const totalAddressInDatabase = await prismaClient.address.count({
+    where: {
+      contact_id: contactId,
+      id: addressId,
+    },
+  });
+
+  if (totalAddressInDatabase !== 1) {
+    throw new ResponseError(404, "Address is not found");
+  }
+
+  return prismaClient.address.delete({
+    where: {
+      id: addressId,
+    },
+  });
+};
+
+export default { create, get, update, remove };
--- test/address.test.js
+++ test/address.test.js
@@ -144,7 +144,109 @@ import { web } from "../src/application/web.js";
 //   });
 // });
 
-describe("PUT /api/contacts/:contactId/addresses/:addressId", function () {
+// describe("PUT /api/contacts/:contactId/addresses/:addressId", function () {
+//   beforeEach(async () => {
+//     await createTestUser();
+//     await createTestContact();
+//     await createTestAddress();
+//   });
+
+//   afterEach(async () => {
+//     await removeAllTestAddresses();
+//     await removeAllTestContact();
+//     await removeTestUser();
+//   });
+
+//   it("should can update address", async () => {
+//     const testContact = await getTestContact();
+//     const testAddress = await getTestAddress();
+
+//     const result = await supertest(web)
+//       .put("/api/contacts/" + testContact.id + "/addresses/" + testAddress.id)
+//       .set("Authorization", "test")
+//       .send({
+//         street: "Jalan",
+//         city: "Kota",
+//         province: "Provinsi",
+//         country: "Negara",
+//         postal_code: "12345678",
+//       });
+
+//     expect(result.status).toBe(200);
+//     expect(result.body.data.id).toBe(testAddress.id);
+//     expect(result.body.data.street).toBe("Jalan");
+//     expect(result.body.data.city).toBe("Kota");
+//     expect(result.body.data.province).toBe("Provinsi");
+//     expect(result.body.data.country).toBe("Negara");
+//     expect(result.body.data.postal_code).toBe("12345678");
+//   });
+
+//   it("should reject if request is not valid", async () => {
+//     const testContact = await getTestContact();
+//     const testAddress = await getTestAddress();
+
+//     const result = await supertest(web)
+//       .put("/api/contacts/" + testContact.id + "/addresses/" + testAddress.id)
+//       .set("Authorization", "test")
+//       .send({
+//         street: "Jalan",
+//         city: "Kota",
+//         province: "Provinsi",
+//         country: "",
+//         postal_code: "",
+//       });
+
+//     expect(result.status).toBe(400);
+//   });
+
+//   it("should reject if address is not valid", async () => {
+//     const testContact = await getTestContact();
+//     const testAddress = await getTestAddress();
+
+//     const result = await supertest(web)
+//       .put(
+//         "/api/contacts/" +
+//           testContact.id +
+//           "/addresses/" +
+//           (testAddress.id + 1),
+//       )
+//       .set("Authorization", "test")
+//       .send({
+//         street: "Jalan",
+//         city: "Kota",
+//         province: "Provinsi",
+//         country: "Negara",
+//         postal_code: "12345678",
+//       });
+
+//     expect(result.status).toBe(404);
+//   });
+
+//   it("should reject if address is not valid", async () => {
+//     const testContact = await getTestContact();
+//     const testAddress = await getTestAddress();
+
+//     const result = await supertest(web)
+//       .put(
+//         "/api/contacts/" +
+//           (testContact.id + 1) +
+//           "/addresses/" +
+//           testAddress.id,
+//       )
+//       .set("Authorization", "test")
+//       .send({
+//         street: "Jalan",
+//         city: "Kota",
+//         province: "Provinsi",
+//         country: "Negara",
+//         postal_code: "12345678",
+//       });
+
+//     expect(result.status).toBe(404);
+//   });
+// });
+
+describe("DELETE /api/contacts/:contactId/addresses/:addressId", function () {
   beforeEach(async () => {
     await createTestUser();
     await createTestContact();
@@ -157,90 +259,51 @@ describe("PUT /api/contacts/:contactId/addresses/:addressId", function () {
     await removeTestUser();
   });
 
-  it("should can update address", async () => {
+  it("should can remove address", async () => {
     const testContact = await getTestContact();
-    const testAddress = await getTestAddress();
+    let testAddress = await getTestAddress();
 
     const result = await supertest(web)
-      .put("/api/contacts/" + testContact.id + "/addresses/" + testAddress.id)
-      .set("Authorization", "test")
-      .send({
-        street: "Jalan",
-        city: "Kota",
-        province: "Provinsi",
-        country: "Negara",
-        postal_code: "12345678",
-      });
+      .delete(
+        "/api/contacts/" + testContact.id + "/addresses/" + testAddress.id,
+      )
+      .set("Authorization", "test");
 
     expect(result.status).toBe(200);
-    expect(result.body.data.id).toBe(testAddress.id);
-    expect(result.body.data.street).toBe("Jalan");
-    expect(result.body.data.city).toBe("Kota");
-    expect(result.body.data.province).toBe("Provinsi");
-    expect(result.body.data.country).toBe("Negara");
-    expect(result.body.data.postal_code).toBe("12345678");
+    expect(result.body.data).toBe("OK");
+
+    testAddress = await getTestAddress();
+    expect(testAddress).toBeNull();
   });
 
-  it("should reject if request is not valid", async () => {
+  it("should reject if address is not found", async () => {
     const testContact = await getTestContact();
-    const testAddress = await getTestAddress();
+    let testAddress = await getTestAddress();
 
     const result = await supertest(web)
-      .put("/api/contacts/" + testContact.id + "/addresses/" + testAddress.id)
-      .set("Authorization", "test")
-      .send({
-        street: "Jalan",
-        city: "Kota",
-        province: "Provinsi",
-        country: "",
-        postal_code: "",
-      });
-
-    expect(result.status).toBe(400);
-  });
-
-  it("should reject if address is not valid", async () => {
-    const testContact = await getTestContact();
-    const testAddress = await getTestAddress();
-
-    const result = await supertest(web)
-      .put(
+      .delete(
         "/api/contacts/" +
           testContact.id +
           "/addresses/" +
           (testAddress.id + 1),
       )
-      .set("Authorization", "test")
-      .send({
-        street: "Jalan",
-        city: "Kota",
-        province: "Provinsi",
-        country: "Negara",
-        postal_code: "12345678",
-      });
+      .set("Authorization", "test");
 
     expect(result.status).toBe(404);
   });
 
-  it("should reject if address is not valid", async () => {
+  it("should reject if contact is not found", async () => {
     const testContact = await getTestContact();
-    const testAddress = await getTestAddress();
+    let testAddress = await getTestAddress();
 
     const result = await supertest(web)
-      .put(
+      .delete(
         "/api/contacts/" +
           (testContact.id + 1) +
           "/addresses/" +
           testAddress.id,
       )
-      .set("Authorization", "test")
-      .send({
-        street: "Jalan",
-        city: "Kota",
-        province: "Provinsi",
-        country: "Negara",
-        postal_code: "12345678",
-      });
+      .set("Authorization", "test");
 
     expect(result.status).toBe(404);
   });
